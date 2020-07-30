b5af525077c0418b7cab1284a56814190c87873a
bk <bk@naver.com>
2020-07-30 19:19:28 +0900

    상세보기 액티비티 진행중

---
+++ opgg/app/src/main/java/com/jaybon/opgg/activityInfoDetail.java
@@ -0,0 +1,14 @@
+package com.jaybon.opgg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activityInfoDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_detail);
+    }
+}
