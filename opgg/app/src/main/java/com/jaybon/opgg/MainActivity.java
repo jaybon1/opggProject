c68044191dfd8119a628819bd6195c40eb2b5e57
unknown <ponggas2@gmail.com>
2020-07-31 17:47:38 +0900

    바텀 네비게이션 제작중

--- opgg/app/src/main/java/com/jaybon/opgg/MainActivity.java
+++ opgg/app/src/main/java/com/jaybon/opgg/MainActivity.java
@@ -1,15 +1,46 @@
 package com.jaybon.opgg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "Main_Activity";
+    private BottomNavigationView bottomNavSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavSearch = findViewById(R.id.bottom_nav_search);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new SearchFragment()).commit();
+
+        bottomNavSearch.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Log.d(TAG, "onNavigationItemSelected: " + item.getItemId());
+
+                switch (item.getItemId()){
+                    case R.id.bottom_nav_search_button:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new SearchFragment()).commit();
+                        break;
+                    case R.id.bottom_nav_ranking_button:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new RankFragment()).commit();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
     }
 }
